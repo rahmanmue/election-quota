39137b7bb8b2094bf63cb0ee7942bbf5856d96a0
rahman.muraman <rahman.muraman@gmail.com>
2024-09-27 11:22:47 +0700

    feat: add package & implement shadcn in main

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,79 @@
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Private from "./components/Private";
-import PrivateRoute from "./routes/PrivateRoute";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import { AuthContextProvider } from "./contexts/AuthContext";
-import ErrorPage from "./components/ErrorPage";
-import Home from "./components/Home";
-import GoogleCallback from "./components/GoogleCallback";
-
-const Auth: React.FC = () => {
-  return (
-    <AuthContextProvider>
-      <Outlet />
-    </AuthContextProvider>
-  );
-};
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register";
+import Private from "./components/Private";
+import PrivateRoute from "./components/PrivateRoute";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import GoogleCallback from "./pages/Login/GoogleCallback";
+import Parpol from "./pages/Parpol/Parpol";
+import Dapil from "./pages/Daerah-Pemilihan/Dapil";
+import Users from "./pages/Users/Users";
+import Profile from "./pages/Profile/Profile";
+import DapilHasilSuara from "./pages/Data-Suara/DapilHasilSuara";
+import FormSuara from "./pages/Data-Suara/FormSuara";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import "./App.css";
 
 const routes = [
   {
-    id: "root",
-    path: "",
-    element: <Auth />,
+    path: "/",
     errorElement: <ErrorPage />,
+    element: <Outlet />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <Home />,
       },
       {
-        path: "/login",
+        path: "login",
         element: <Login />,
       },
       {
-        path: "/google/callback",
+        path: "google/callback",
         element: <GoogleCallback />,
       },
       {
-        path: "/register",
+        path: "register",
         element: <Register />,
       },
       {
-        path: "/",
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "partai-politik",
+        element: <Parpol />,
+      },
+      {
+        path: "daerah-pemilihan",
+        element: <Dapil />,
+      },
+      {
+        path: "daerah-pemilihan/:id",
+        element: <DapilHasilSuara />,
+      },
+      {
+        path: "daerah-pemilihan/tambah-suara/:id",
+        element: <FormSuara />,
+      },
+      {
+        path: "daerah-pemilihan/edit-suara/:id",
+        element: <DapilHasilSuara />,
+      },
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
         element: <PrivateRoute roles={["admin"]} />,
         children: [
           {
-            path: "/private",
+            path: "private",
             element: <Private />,
           },
         ],
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "./contexts/ThemeProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
