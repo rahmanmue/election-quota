ee2ce0e525682555f43124f8c9c56e9124be71a6
rahman.muraman <rahman.muraman@gmail.com>
2024-10-01 00:23:07 +0700

    feat: update store authSlice

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -5,6 +5,7 @@ import axiosInstance from "@/services/api";
 export interface AuthState {
   token: string | null;
   isAuthenticated: boolean;
+  userId?: string;
 }
 
 const initialState: AuthState = {
@@ -16,14 +17,19 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    loginSuccess: (state, action: PayloadAction<{ token: string }>) => {
+    loginSuccess: (
+      state,
+      action: PayloadAction<{ token: string; userId: string }>
+    ) => {
       state.token = action.payload.token;
+      state.userId = action.payload.userId;
       state.isAuthenticated = true;
       axiosInstance.defaults.headers.common.Authorization = `Bearer ${state.token}`;
       localStorage.setItem(
         "authState",
         JSON.stringify({
           token: state.token,
+          userId: state.userId,
           isAuthenticated: state.isAuthenticated,
         })
       );
